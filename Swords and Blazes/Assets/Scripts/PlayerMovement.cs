157c867ae31496c411b28469439132ac9b3fad00
LelouchVDK <christian.rettrup@gmail.com>
2023-10-06 23:49:46 +0200

    Atttempt with gravity as movement

--- Swords and Blazes/Assets/Scripts/PlayerMovement.cs
+++ Swords and Blazes/Assets/Scripts/PlayerMovement.cs
@@ -5,29 +5,62 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float player_movementX;
-    float player_movementY;
+
+    enum GravityDirection
+    {
+        Down,
+        Left,
+        Right,
+        Up
+    }
+
+    GravityDirection m_playerMovement;
 
     Rigidbody2D rb;
-    
-    
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        m_playerMovement = GravityDirection.Down;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        
+        switch (m_playerMovement) 
+        { 
+            case GravityDirection.Left:
+                Physics2D.gravity = new Vector2(-10f, 0);
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    m_playerMovement = GravityDirection.Left;
+                }
+                break;
+
+            case GravityDirection.Right:
+                Physics2D.gravity = new Vector2(10f, 0);
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    m_playerMovement = GravityDirection.Right;
+                }
+                break;    
+
+            case GravityDirection.Up:
+                Physics2D.gravity = new Vector2(0, 10f);
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    m_playerMovement = GravityDirection.Up;
+                }
+                break;
+
+            case GravityDirection.Down:
+                Physics2D.gravity = new Vector2(0, -10f);
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    m_playerMovement = GravityDirection.Down;
+                }
+                break;
+        }
     }
 
-    void OnMove(InputValue moveValue)
-    {
-        Vector2 moveVector = moveValue.Get<Vector2>();
-
-        player_movementX = moveVector.x;
-        player_movementY = moveVector.y;
-    }
+    
 }
