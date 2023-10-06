0ded3c1db398b20d326c58cdabee0f8ab4ca82ca
LelouchVDK <christian.rettrup@gmail.com>
2023-10-06 14:08:01 +0200

    Made movement

--- Swords and Blazes/Assets/Scripts/PlayerMovement.cs
+++ Swords and Blazes/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float player_movementX;
+    float player_movementY;
+
+    Rigidbody2D rb;
+    
+    
     // Start is called before the first frame update
     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue moveValue)
     {
-        
+        Vector2 moveVector = moveValue.Get<Vector2>();
+
+        player_movementX = moveVector.x;
+        player_movementY = moveVector.y;
     }
 }
