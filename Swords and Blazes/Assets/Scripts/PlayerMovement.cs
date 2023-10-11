1bc43884d92a6b301c2dc08df1284d8b67a665c8
Lotta20 <lottageldschlaeger@gmail.com>
2023-10-11 10:26:29 +0200

    Revert "Merge branch 'Movement' into Obstacles-andcollectibles"
    This reverts commit 520feb85fd9bce86de201456db042ac12123309b, reversing
    changes made to 91cbe1599a2e2e43f088ef51533a9babba1a0398.

--- Swords and Blazes/Assets/Scripts/PlayerMovement.cs
+++ Swords and Blazes/Assets/Scripts/PlayerMovement.cs
@@ -1,55 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    
-
-    
-
-    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {     
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                Vector2 right = new Vector2(100f, 0);
-                rb.AddForce(right);
-                
-            }
-            
-
-                
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                Vector2 left = new Vector2(-100f, 0);
-                rb.AddForce(left);
-                
-            } 
-
-                
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                Vector2 up = new Vector2(0, 100f);
-                rb.AddForce(up);
-                
-            }
-                
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                Vector2 down = new Vector2(0, -100f);
-                rb.AddForce(down);
-                
-            }
-            
+    void Update()
+    {
+        
     }
-    }
+}
