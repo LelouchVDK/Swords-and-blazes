828e8a940990d7fa7bbfdf6adb71c09f113f94d3
Mwb2110 <Martin.W.Busk@live.dk>
2023-10-11 11:14:23 +0200

    Added gradient to Healthbars

--- Swords and Blazes/Assets/Scripts/HealthBar2.cs
+++ Swords and Blazes/Assets/Scripts/HealthBar2.cs
@@ -6,15 +6,22 @@ using UnityEngine.UI;
 public class HealthBar2 : MonoBehaviour
 {
     public Slider slider2;
+    public Gradient gradient2;
+    public Image fill2;
 
     public void SetMaxHealth(int health)
     {
         slider2.maxValue = health;
         slider2.value = health;
+
+        fill2.color = gradient2.Evaluate(1f);
+
     }
 
     public void SetHealth(int health)
     {
         slider2.value = health;
+
+        fill2.color = gradient2.Evaluate(slider2.normalizedValue);
     }
 }
