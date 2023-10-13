1b27c67a7a918016b480fd8dc79ac28d3354d9c9
Lotta20 <lottageldschlaeger@gmail.com>
2023-10-13 10:19:14 +0200

    Added coincounter for tredje gang

--- Swords and Blazes/Assets/Scripts/Collectibles.cs
+++ Swords and Blazes/Assets/Scripts/Collectibles.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collectibles : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private int coins = 0;
+    [SerializeField] private Text CoinCounter;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -24,8 +26,8 @@ public class Collectibles : MonoBehaviour
         {
             Destroy(collision.gameObject);
             coins++;
-            Debug.Log("Coins: " + coins);
+            CoinCounter.text = "Coins " + coins;
         }
     }
 
-}
+} 
