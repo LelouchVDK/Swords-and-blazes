baf0fa469543b8045e97a3b267eca06e6959430c
LelouchVDK <christian.rettrup@gmail.com>
2023-11-03 11:05:31 +0100

    Face aim

--- Swords and Blazes/Assets/Scripts/Shoot.cs
+++ Swords and Blazes/Assets/Scripts/Shoot.cs
@@ -10,12 +10,14 @@ public class Shoot : MonoBehaviour
     public GameObject cowboy;
     public LineRenderer render;
     public Vector2 offset;
+    public CowboyMovement faceDir;
     
 
     // Start is called before the first frame update
     void Start()
     {
         Vector2 mouse_pos = Input.mousePosition;
+        CowboyMovement faceDir = cowboy.GetComponent<CowboyMovement>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,10 @@ public class Shoot : MonoBehaviour
         {
             Instantiate(bullet, shootingPoint.position,transform.rotation);
         }
+        if (!faceDir.faceLeft)
+        {
+            offset = -offset;
+        }
         Vector2 screen_pos = Input.mousePosition;
         Vector2 world_pos = Camera.main.ScreenToWorldPoint(screen_pos);
         Aim(world_pos);
@@ -37,9 +43,9 @@ public class Shoot : MonoBehaviour
         Vector2 origin = (Vector2) transform.position + offset;
         Vector2 dir = target - origin;
         RaycastHit2D ray = Physics2D.Raycast(origin, dir);
-        RaycastHit2D[] hits = Physics2D.RaycastAll(origin, dir);
-        hits[0].collider.GetComponent<HealthBar>();
-        Debug.Log(string.Join(", ", hits));
+        //RaycastHit2D[] hits = Physics2D.RaycastAll(origin, dir);
+        //hits[0].collider.GetComponent<HealthBar>();
+        //Debug.Log(string.Join(", ", hits));
         Vector2 end = ray.point;
         render.SetPosition(0, origin);
         render.SetPosition(1, end);
