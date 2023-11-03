b78431afb31a7292356ce6881ac58628d942ca03
LelouchVDK <christian.rettrup@gmail.com>
2023-11-03 10:07:08 +0100

    Made aim for cowboy

--- Swords and Blazes/Assets/Scripts/Shoot.cs
+++ Swords and Blazes/Assets/Scripts/Shoot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,14 @@ public class Shoot : MonoBehaviour
     public Transform shootingPoint;
     public GameObject bullet;
     public GameObject cowboy;
-    public Vector2 mouse_pos = Input.mousePosition;
+    public LineRenderer render;
+    public Vector2 offset;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 mouse_pos = Input.mousePosition;
     }
 
     // Update is called once per frame
@@ -24,12 +27,22 @@ public class Shoot : MonoBehaviour
         {
             Instantiate(bullet, shootingPoint.position,transform.rotation);
         }
+        Vector2 screen_pos = Input.mousePosition;
+        Vector2 world_pos = Camera.main.ScreenToWorldPoint(screen_pos);
+        Aim(world_pos);
     }
 
-    private void OnDrawGizmos()
+    void Aim(Vector2 target)
     {
-        Gizmos.color = Color.white;
-        Gizmos.DrawLine(cowboy.transform.position, mouse_pos);
+        Vector2 origin = (Vector2) transform.position + offset;
+        Vector2 dir = target - origin;
+        RaycastHit2D ray = Physics2D.Raycast(origin, dir);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(origin, dir);
+        hits[0].collider.GetComponent<HealthBar>();
+        Debug.Log(string.Join(", ", hits));
+        Vector2 end = ray.point;
+        render.SetPosition(0, origin);
+        render.SetPosition(1, end);
 
     }
 }
