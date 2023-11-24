629e45e1fa9495a8a5417c3d84d111407c8c1c13
TootSnoot <victoriakippa@gmail.com>
2023-11-24 12:34:32 +0100

    Added notes to CowboyDamange script

--- Swords and Blazes/Assets/Scripts/CowboyDamage.cs
+++ Swords and Blazes/Assets/Scripts/CowboyDamage.cs
@@ -19,10 +19,14 @@ public class CowboyDamage : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        // If this object collides with another object with the tag "Knight", that object takes damage.
         if (collision.gameObject.tag == "Knight")
         {
             knightHealth.KnightTakeDamage(10);
         }
+
+        // If this object collides with an object with the tag "Cactus", this object takes damage.
         if (collision.gameObject.tag == "Cactus")
         {
             cowboyHealth.CowboyTakeDamage(10);
