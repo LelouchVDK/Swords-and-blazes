fe1de6cb735696d21d2c2e61f060d3f2c5366975
Lotta20 <lottageldschlaeger@gmail.com>
2023-11-28 14:39:57 +0100

    Fixed Collectibles finally

--- Swords and Blazes/Assets/Scripts/Collectibles.cs
+++ Swords and Blazes/Assets/Scripts/Collectibles.cs
@@ -5,10 +5,14 @@ using UnityEngine.UI;
 
 public class Collectibles : MonoBehaviour
 {
+
+    private System.Random rand = new System.Random();
+    SpawnCoin spawner;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        spawner = GameObject.Find("CoinSpawner").GetComponent<SpawnCoin>();
     }
 
     // Update is called once per frame
@@ -17,6 +21,14 @@ public class Collectibles : MonoBehaviour
 
     }
 
+    //IEnumerator Respawn (Collider2D collision, int time)
+    //{
+       // yield return new WaitForSeconds(time);
+
+        //collision.gameObject.SetActive(true);
+    //} 
+
+
     private int coins = 0;
     [SerializeField] private Text CoinCounter;
 
@@ -24,9 +36,16 @@ public class Collectibles : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Coin"))
         {
-            Destroy(collision.gameObject);
+
+            collision.gameObject.SetActive(false);
+            
+            //Destroy(collision.gameObject);//
             coins++;
             CoinCounter.text = "Coins " + coins;
+
+            //StartCoroutine (Respawn(collision,6));
+            spawner.SpawnCoins();
+
         }
     }
 
---
+++ Swords and Blazes/Assets/Scripts/SpawnCoin.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCoin : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Coins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCoins(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnCoins()
+    {
+        bool coinsSpawned = false;
+        while (!coinsSpawned)
+        {
+         Vector3 coinPosition = new Vector3(Random.Range(-7f, 7f), Random.Range(-4f, 4f), 0f);
+
+           
+            
+            
+            
+            if ((coinPosition - transform.position).magnitude < 3)
+            {
+               continue;
+            }
+
+            else
+            {
+                Instantiate(Coins, coinPosition, Quaternion.identity);
+                coinsSpawned = true;
+            }
+        }
+
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag is "Knight" or "Cowboy")
+        {
+            Destroy(collision.gameObject);
+            SpawnCoins();
+        }
+
+        
+    }
+}
+
