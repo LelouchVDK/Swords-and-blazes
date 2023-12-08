7811ed5338061df49fbbfce67b0724e26a7efbb5
LelouchVDK <christian.rettrup@gmail.com>
2023-12-08 11:26:33 +0100

    Added documentation

--- Swords and Blazes/Assets/Scripts/Buttonhandler.cs
+++ Swords and Blazes/Assets/Scripts/Buttonhandler.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Script for button events
+/// </summary>
 public class Buttonhandler : MonoBehaviour
 {
     
