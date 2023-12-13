379d70c3d85def0e5675895e8fe54751c4cdbb9e
Mwb2110 <Martin.W.Busk@live.dk>
2023-12-13 09:29:38 +0100

    Added coin sound

--- Swords and Blazes/Assets/Scripts/CowboySounds.cs
+++ Swords and Blazes/Assets/Scripts/CowboySounds.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CowboySounds : MonoBehaviour
 {
-
+    public AudioClip Coin;
     private AudioSource cowboyShoot;
     // Start is called before the first frame update
     void Start()
@@ -22,6 +22,14 @@ public class CowboySounds : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             cowboyShoot.Stop();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            cowboyShoot.clip = Coin;
+            cowboyShoot.Play();
         }
     }
 }
--- Swords and Blazes/Assets/Scripts/KnightSounds.cs
+++ Swords and Blazes/Assets/Scripts/KnightSounds.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class KnightSounds : MonoBehaviour
 {
     public AudioClip sword;
+    public AudioClip Coin;
     private AudioSource knightSlash;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,15 @@ public class KnightSounds : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             knightSlash.clip = sword;
+            knightSlash.Play();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            knightSlash.clip = Coin;
             knightSlash.Play();
         }
     }
